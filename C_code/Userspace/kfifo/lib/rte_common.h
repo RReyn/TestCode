4ab1b5ea01cfda75d3609e78db86371ba5c1eb79
root <root@localhost.localdomain>
2017-02-20 21:05:32 +0800

    Add kfifo-userspace Code

---
+++ C_code/Userspace/kfifo/lib/rte_common.h
@@ -0,0 +1,74 @@
+#ifndef __COMMON_H__
+#define __COMMON_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define __must_check	__attribute__((warn_unused_result))
+
+/*
+ * Macro to return the minimum of two numbers
+ */
+#define min(a, b) ({ \
+		typeof (a) _a = (a); \
+		typeof (b) _b = (b); \
+		_a < _b ? _a : _b; \
+	})
+
+#define max(a, b) ({ \
+		typeof (a) _a = (a); \
+		typeof (b) _b = (b); \
+		_a > _b ? _a : _b; \
+	})
+
+/* Memory barrier: FULL barrier */
+#define MB()	__sync_synchronize()
+
+
+/**
+ * Returns true if n is a power of 2
+ * @param n
+ * 	Number to check
+ *
+ * @return
+ * 	1 if true, 0 otherwise
+ */
+static inline int
+is_power_of_2(uint32_t n)
+{
+	return n && !(n & (n - 1));
+}
+
+/**
+ * Aligns input parameter to the next power of 2
+ *
+ * @param x
+ * 	The integer value to align
+ *
+ * @return
+ * 	Input parameter aligned to the next power of 2
+ */
+static inline uint32_t
+align32pow2(uint32_t x)
+{
+	x--;
+	x |= x >> 1;
+	x |= x >> 2;
+	x |= x >> 4;
+	x |= x >> 8;
+	x |= x >> 16;
+	
+	return x + 1;
+}
+
+static inline void
+rte_pause(void)
+{
+	/* Do nothing */
+}
+
+#ifdef __cplusplus
+}
+#endif
+#endif /* COMMON_H END*/
