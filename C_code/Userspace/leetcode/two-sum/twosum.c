83c9a1b15a11cc1fc1d9d7f41f168574dd073127
ry0117 <ry0117@163.com>
2018-02-09 11:12:36 +0800

    add makefile and main function

--- C_code/Userspace/leetcode/two-sum/twosum.c
+++ C_code/Userspace/leetcode/two-sum/twosum.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+#include <stdlib.h>
 /**
  *  * Note: The returned array must be malloced, assume caller calls free().
  *   */
@@ -18,4 +20,35 @@ twoSum(int* nums, int numsSize, int target) {
 		}
 	}
 	return NULL;
+}
+
+int
+main(int argc, char *argv[])
+{
+	int nums[] = {2, 7, 11, 15};
+	int *result;
+
+	result = twoSum(nums, 4, 9);
+	if (result == NULL)
+		return 1;
+	printf("result1: [%d,%d]\n", result[0], result[1]);
+	free(result);
+	
+	result = twoSum(nums, 4, 13);
+	if (result == NULL)
+		return 1;
+	printf("result2: [%d,%d]\n", result[0], result[1]);
+	free(result);
+	result = twoSum(nums, 4, 18);
+	if (result == NULL)
+		return 1;
+	printf("result3: [%d,%d]\n", result[0], result[1]);
+	free(result);
+	result = twoSum(nums, 4, 22);
+	if (result == NULL)
+		return 1;
+	printf("result4: [%d,%d]\n", result[0], result[1]);
+	free(result);
+
+	return 0;
 }
