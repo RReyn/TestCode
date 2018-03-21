4d7176eb29bcf5aa9140ed58279785b9ac78972f
ry0117 <ry0117@163.com>
2018-03-21 16:16:12 +0800

    add the function set_fd_nonblock in header file

--- C_code/Userspace/ldetect/include/lib/io_wrapper.h
+++ C_code/Userspace/ldetect/include/lib/io_wrapper.h
@@ -11,6 +11,7 @@ extern int open_send_socket(sa_family_t family,
 	int proto, const char *devname);
 extern int open_read_socket(sa_family_t family,
 	int proto, const char *devname);
+extern int set_fd_nonblock(int fd);
 
 #ifdef __cplusplus
 }
